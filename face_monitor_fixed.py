81865146e9917b02903c3da85c9e5e1190d7d143
Ashlin Joel Sizzin <142215124+AshlinJoelSizzin@users.noreply.github.com>
2025-12-27 14:05:58 +0530

    Update face_monitor_fixed.py

--- face_monitor_fixed.py
+++ face_monitor_fixed.py
@@ -11,7 +11,7 @@ import albumentations as A
 import hashlib
 import bcrypt
 
-DB_URL = "postgresql://postgres:AshlinJoelSizzin@localhost:5432/face_detection_db"  # <-- PUT your password!
+DB_URL = "postgresql://postgres:Password@localhost:5432/face_detection_db"  # <-- PUT your password!
 USER_IMG_DIR = "user_images"
 ACTOR_IMG_DIR = "actor_uploads"
 YOLOV8_FACE_WEIGHTS = "yolov8x-face-lindevs.pt"
